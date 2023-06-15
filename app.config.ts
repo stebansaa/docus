f235a97ca9dfdb5286fa5a6c877bec14c4f8eab8
Esteban Saá <steban@gmail.com>
2023-06-14 23:19:34 -0400

    updates

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Frogge.Finance documentation',
+    description: 'Learn abou Frogge Finance.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxtstudio',
-      github: 'nuxt-themes/docus'
+      twitter: 'froggefinance',
+      github: 'evm20/'
     },
     github: {
       dir: '.starters/default/content',
@@ -24,8 +24,8 @@ export default defineAppConfig({
       fluid: true
     },
     header: {
-      logo: true,
-      showLinkIcon: true,
+      logo: false,
+      showLinkIcon: false,
       exclude: [],
       fluid: true
     },
